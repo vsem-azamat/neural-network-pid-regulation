09bba160411bb9df687d9e21ed101a61cb5a90c5
Azamat Almazbek uulu <almazbek1608@gmail.com>
2024-05-11 20:16:01 +0200

    chore: Refactor Trolley class and update initialization parameters

--- systems/trolley.py
+++ systems/trolley.py
@@ -5,21 +5,22 @@ from .base import BaseSystem
 
 
 class Trolley(BaseSystem):
-	def __init__(self, mass: Tensor, friction: Tensor, dt: Tensor) -> None:
+	def __init__(self, 
+			mass: Tensor,
+			spring: Tensor,
+			friction: Tensor,
+			dt: Tensor
+			) -> None:
 		"""
-		Initialize the trolley
-
 		Args:
 			mass (float): mass of the trolley
+			spring (float): spring constant of the trolley
 			friction (float): friction coefficient of the trolley
 			dt (float): time step between the current and previous position
-
-		Returns:
-			None
 		"""
 		self.mass = mass
 		self.friction = friction
-		self.spring_constant = torch.tensor(5.)
+		self.spring = spring
 		self.dt = dt
 
 		self.position = torch.tensor(0.)
@@ -32,56 +33,35 @@ class Trolley(BaseSystem):
 		"""
 		Update the position and velocity of the trolley based on the control output
 		
-		Args:
-			TODO: select best unit for the demonstration
-		
-		Returns:
-			None
-		
 		Equation of model:
             F = ma
             a = F/m
-            a = F/m - friction*v/m - spring_constant*x/m
+            a = F/m - friction*v/m - spring_constant*x/m - disturbance/m
             v = v + a*dt
             x = x + v*dt
 		"""
 		assert control_output is not None, "Control output is None"
 		F = control_output
-		acceleration = F / self.mass - self.friction * self.velocity.detach() / self.mass - self.spring_constant * self.position.detach() / self.mass
+		acceleration = F / self.mass - \
+			self.friction * self.velocity.detach() / self.mass - \
+			self.spring * self.position.detach() / self.mass - \
+			distrubance / self.mass
 		self.velocity = self.velocity.detach() + acceleration * self.dt
 		self.position = self.position.detach() + self.velocity * self.dt
 		return self.position
 
 
 	def get_position(self) -> Tensor:
-		"""
-		Get the position of the trolley
-
-		Args:
-			None
-
-		Returns:
-			float: position of the trolley
-		"""
 		return self.position
-	
+
 
 	def get_U(self) -> tuple[Tensor, Tensor, Tensor]:
 		return self.position, self.position/self.dt, self.position/(self.dt**2)
 	
 
 	def reset(self) -> None:
-		"""
-		Reset the position and velocity of the trolley
-
-		Args:
-			None
-
-		Returns:
-			None
-		"""
+		"""Reset: position, velocity, and delta_position to zero"""
 		self.position = torch.tensor(0, dtype=torch.float32)
 		self.velocity = torch.tensor(0, dtype=torch.float32)
 		self.delta_position = torch.tensor(0, dtype=torch.float32)
-
 
