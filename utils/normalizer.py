13ca7daf80a534c701adfcff010ce43ddb3608f4
vsem-azamat <almazbek1608@gmail.com>
2024-08-06 08:17:31 +0200

    refactor: Normalize data using mean and std in Normalizer class

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -1,5 +1,11 @@
 
 class Normalizer:
+    """
+    Normalizer class to normalize and denormalize data using mean and std.
+
+    mean = sum(x) / N
+    std = sqrt(sum((x - mean)^2) / N)
+    """
     def __init__(self, data):
         self.mean = data.mean(dim=0)
         self.std = data.std(dim=0, unbiased=False)
