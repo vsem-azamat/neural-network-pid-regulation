23b503215b62824c2bb6e19675f87c64cf7e454f
vsem-azamat <almazbek1608@gmail.com>
2024-10-14 12:12:55 +0200

    fix: Correct typo in weights directory variable name in config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Config:
-    WEGHTS_DIR: str = 'weights'
-    os.makedirs(WEGHTS_DIR, exist_ok=True)
+    WEIGHTS_DIR: str = 'weights'
+    os.makedirs(WEIGHTS_DIR, exist_ok=True)
 
     PUBLIC_DIR: str = 'public'
     os.makedirs(PUBLIC_DIR, exist_ok=True)
