0d13002ad808ae5e46aa3fe11341840befae527a
vsem-azamat <almazbek1608@gmail.com>
2024-10-14 12:11:46 +0200

    feat: Enhance PID class with new compute method and properties for error tracking

--- entities/pid.py
+++ entities/pid.py
@@ -1,5 +1,6 @@
 import torch
 from torch import Tensor
+from typing import Literal
 
 class PID:
 	def __init__(self, initial_KP: Tensor, initial_KI: Tensor, initial_KD: Tensor):
@@ -21,14 +22,40 @@ class PID:
 		self.saturation_max = None
 		self.saturation_min = None
 
+	@property
+	def E(self) -> Tensor:
+		return self.e_k
+	
+	@property
+	def dE(self) -> Tensor:
+		return self.e_k - self.e_k_1
+
 	def update_gains(self, new_Kp: Tensor, new_Ki: Tensor, new_Kd: Tensor) -> None:
 		self.Kp = new_Kp
 		self.Ki = new_Ki
 		self.Kd = new_Kd
 
 
-	def compute(self, error: Tensor, dt: Tensor) -> Tensor:
-		return self.compute_standard(error, dt)
+	def compute(
+		self, 
+		error: Tensor, 
+		dt: Tensor,
+		method: Literal['standard', 'backward_euler', 'trapezoidal', 'forward_euler', 'bilinear_transform'] = 'standard'
+		) -> Tensor:
+		# return self.compute_standard(error, dt)
+		match method:
+			case 'standard':
+				return self.compute_standard(error, dt)
+			case 'backward_euler':
+				return self.compute_backward_euler(error, dt)
+			case 'trapezoidal':
+				return self.compute_trapezoidal(error, dt)
+			case 'forward_euler':
+				return self.compute_forward_euler(error, dt)
+			case 'bilinear_transform':
+				return self.compute_bilinear_transform(error, dt)
+			case _:
+				raise ValueError("Invalid method. Choose between 'standard', 'backward_euler', 'trapezoidal', 'forward_euler', 'bilinear_transform'")
 
 
 	def compute_standard(self, error: Tensor, dt: Tensor) -> Tensor:
@@ -41,7 +68,7 @@ class PID:
 			  self.Ki * self.e_k * dt + \
 			  self.Kd * ((self.e_k - self.e_k_1) - (self.e_k_1 - self.e_k_2)) / dt
 
-		self.u_k_1 = u_k.detach()
+		self.u_k_1 = u_k.clone().detach()
 
 		if isinstance(self.saturation_max, Tensor) and isinstance(self.saturation_min, Tensor):
 			return torch.clamp(u_k, self.saturation_min, self.saturation_max)
