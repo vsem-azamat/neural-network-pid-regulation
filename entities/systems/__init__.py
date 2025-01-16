74f6b8c05c5631b6add2e3ce9a72e4bee2e8b005
vsem-azamat <almazbek1608@gmail.com>
2025-01-16 13:11:12 +0100

    feat: remove in progress code base. That all is saved in dev branch

--- entities/systems/__init__.py
+++ entities/systems/__init__.py
@@ -1,6 +1,3 @@
 from .base import BaseSystem
 from .trolley import Trolley
-from .tankheating import TankHeating
-from .springdamper import SpringDamper
-from .pendulum import NonLinearPendulumCart
 from .thermal import Thermal
